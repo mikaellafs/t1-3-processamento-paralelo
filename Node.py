807cc910ae7040b4e7ef9c53381c4ce751d0bcea
mikaellafs <mikaellaferreira0@gmail.com>
2021-08-17 18:50:51 -0300

    problema de receber o id dos 8 nós resolvido?

--- Node.py
+++ Node.py
@@ -1,14 +1,19 @@
 import paho.mqtt.client as mqtt
 from random import randrange
 import sys
+import numpy as np
+from time import sleep
 
-
-# MUITA COISA PRA TRATAR AINDA
-
+nodes = np.array([]) # para guardar os nodesId
 
 def on_message(client, userdata, msg):
     topic = msg.topic
-    m = message.payload.decode("utf-8")
+    m = msg.payload.decode("utf-8")
+
+    if(topic == "join"):
+        m = int(m) # é um node id, converte pra int
+        nodes = np.unique(np.append(nodes, m))  #insere no array de nós, o unique retira repetidos
+    # else
     # se topic = put ou get fazer:
     # client.publish("putOk", True)  --- sei la o que retornar??
     # client.publish("getOk", hashTable[int(m)]) --se tiver dentro da faixa de posicoes que ele ta responsavel
@@ -24,12 +29,18 @@ mqttBroker = "127.0.0.1"
 client = mqtt.Client("Node_%s" % sys.argv) # passar como parametro o nome/numero do nó
 client.connect(mqttBroker)
 
-# Publica seu nodeID no topic "join"
-client.publish("join", nodeID)
-
 # Se inscreve para receber os outros 7 nodeID's
 client.subscribe("join")
 client.on_message = on_message
+
+# Publica seu nodeID no topic "join"
+while(nodes.size < 8): # total de nós
+    client.publish("join", nodeID)
+    sleep(0.5)# espera um tempo
+
+# Obtem antecessor e sucessor
+index = np.where(nodes == nodeID)
+ant, suc = (index -1, index +1) # antecessor e sucessor
 
 # Se inscreve no topico put e get
 client.subscribe("put")
