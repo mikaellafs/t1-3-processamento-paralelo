a447a5adcb0c09f5953fdc5eff61c05381ceb449
leodeorce <42984982+leodeorce@users.noreply.github.com>
2021-09-01 20:31:11 -0300

    Comentários

--- Node.py
+++ Node.py
@@ -4,17 +4,17 @@ import sys
 import numpy as np
 from time import sleep
 
-total_nodes = 8       # total de nós na DHT
-nodes = np.array([])  # para guardar os nodesId
-ack_nodes = np.array([])  # para guardar os nós prontos
+total_nodes = 8       # Total de nós na DHT
+nodes = np.array([])  # Para guardar os nodeIDs
+ack_nodes = np.array([])  # Para guardar os nós prontos
 
 
 def check_interval(k):
     k = int(k)
-    if index == 0:  # se é o primeiro nó do intervalo, seu antecessor possui nodeId menor que o seu
+    if index == 0:  # Se é o primeiro nó do intervalo, seu antecessor possui nodeID menor que o seu
         return (nodes[ant] < k < rangeAddr) or (0 < k <= nodes[index])
 
-    return nodes[ant] < k <= nodes[index]  # checa se esta dentro do intervalo de responsabilidade
+    return nodes[ant] < k <= nodes[index]  # Checa se esta dentro do intervalo de responsabilidade
 
 
 def on_message(client, userdata, msg):
@@ -23,16 +23,16 @@ def on_message(client, userdata, msg):
 
     if topic == "join":
         global nodes
-        m = int(m)  # é um node id, converte pra int
+        m = int(m)  # m é um nodeID, converte pra int
         nodes = np.unique(np.append(nodes, m))  # insere no array de nós, o unique retira repetidos
 
     elif topic == "ack-join":
         global ack_nodes
-        m = int(m)  # é um node id, converte pra int
+        m = int(m)  # m é um nodeID, converte pra int
         ack_nodes = np.unique(np.append(ack_nodes, m))  # o nó em questao que fez o ack esta pronto
 
     elif topic == "put":
-        key, value = m.split(" ", 1)  # é uma mensagem com "chave string"
+        key, value = m.split(" ", 1)  # m é uma mensagem no formato "chave string"
         if check_interval(key):
             key = int(key)
             hashTable[key] = value
@@ -40,26 +40,27 @@ def on_message(client, userdata, msg):
             print("Valor %s armazenado com sucesso na chave %s." % (value, str(key)))
         
     else:  # get
-        if check_interval(m):  # recebe uma chave
+        if check_interval(m):  # Recebe uma chave
             key = int(m)
             value = hashTable[key]
             client.publish("res-get", value)
             print("Valor %s retornado da chave %s." % (value, str(key)))
 
 
-rangeAddr = 2 ** 32
+rangeAddr = 2 ** 32  # Quantidade máxima de endereços na tabela hash
 hashTable = {}
 nodeID = randrange(0, rangeAddr)
 
-mqttBroker = "127.0.0.1"
+mqttBroker = "127.0.0.1"  # Broker tem IP local e porta padrão
 
+# Verifica se foi passado um nome/número específico para o node
 if len(sys.argv) > 1:
     name = sys.argv[1]
 else:
     name = nodeID
 
 # Conecta ao broker mqtt
-client = mqtt.Client("Node_%s" % name)  # passar como parametro o nome/numero do nó
+client = mqtt.Client("Node_%s" % name)  # Passar como parâmetro o nome/número do nó
 while client.connect(mqttBroker) != 0:
     sleep(0.1)
 print("Node_%s conectado ao broker." % name, "ID: " + str(nodeID))
@@ -72,13 +73,13 @@ client.loop_start()
 
 print("Preparando para receber mensagens...")
 # Publica seu nodeID no topic "join"
-while nodes.size < total_nodes or ack_nodes.size < total_nodes:  # enquanto nao esta pronto ou os outros nós nao estao
+while nodes.size < total_nodes or ack_nodes.size < total_nodes:  # Enquanto não está pronto ou os outros nós não estão
     client.publish("join", nodeID)
 
-    if nodes.size == total_nodes:  # nó está pronto, logo avisa aos outros
+    if nodes.size == total_nodes:  # Nó está pronto, logo avisa aos outros
         client.publish("ack-join", nodeID)
 
-    sleep(0.5)  # espera um tempo
+    sleep(0.5)  # Espera um tempo
 
 # Obtem antecessor e sucessor
 nodes.sort()
@@ -88,7 +89,7 @@ if suc == total_nodes:
     suc = 0
 
 print("Pronto! Intervalo de responsabilidade: (", int(nodes[ant]), ",", int(nodes[index]), "]")
-# Se inscreve no topico put e get
+# Se inscreve no tópico put e get
 client.subscribe("put")
 client.subscribe("get")
 
